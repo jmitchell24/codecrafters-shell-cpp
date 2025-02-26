6aaa333fbb6b421128d1dffadfd5989e6ede4051
James Mitchell <mail@jpmitchell.ca>
2025-02-26 08:28:04 -0500

    pass 2st stage

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,85 @@
 #include <iostream>
+#include <cstdlib>
+#include <span>
+#include <charconv>
+
+using namespace std;
+
+#include <ut/string.hpp>
+using namespace ut;
+
+using arglist_t = span<strview>;
+
+void cmdExit(arglist_t const& args)
+{
+    int code = EXIT_FAILURE;
+
+    if (args.size() != 1)
+    {
+        auto&& a = args[0];
+        from_chars(a.begin(), a.end(), code);
+    }
+
+    exit(code);
+}
+
+void parseCommand(arglist_t const& args)
+{
+    if (args.empty())
+        return;
+
+    auto&& command = args[0];
+    if (command == "exit"_sv)
+    {
+        cmdExit(args.subspan(1));
+    }
+    else
+    {
+
+    }
+}
+
+void parseText(cstrparam s)
+{
+    auto args = trimsplit::containerWS(s);
+    parseCommand(args);
+}
 
 int main()
 {
-    // Flush after every std::cout / std:cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
 
-    // Uncomment this block to pass the first stage
-    std::cout << "$ ";
+    constexpr size_t BUFFER_SIZE = 2048;
+    char buffer[BUFFER_SIZE];
 
-    std::string input;
-    std::getline(std::cin, input);
+    printf("$ ");
+
+    if (fgets(buffer, BUFFER_SIZE, stdin) == nullptr)
+    {
+        printf("something went wrong...\n");
+        return EXIT_FAILURE;
+    }
+
+    auto user_input = cstrview(buffer).trim();
+
+    printf("%.*s: command not found\n", user_input.size(), user_input.begin());
+
+    return EXIT_SUCCESS;
+
+    // while (true)
+    // {
+    //     printf("$ ");
+    //
+    //     if (fgets(buffer, BUFFER_SIZE, stdin) == nullptr)
+    //     {
+    //         printf("something went wrong...\n");
+    //         return EXIT_FAILURE;
+    //     }
+    //
+    //     auto argv = trimsplit::containerWS(cstrview(buffer));
+    //
+    //     parseText(buffer);
+    //
+    // }
+
+
 }
