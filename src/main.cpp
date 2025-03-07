e31acc6399210affeb58bbb2acda6b759355388e
James Mitchell <mail@jpmitchell.ca>
2025-03-07 14:33:19 -0500

    echo builtin

--- src/main.cpp
+++ src/main.cpp
@@ -9,44 +9,77 @@ using namespace std;
 #include <ut/string.hpp>
 using namespace ut;
 
-using arglist_t = span<strview>;
-
-void cmdExit(arglist_t const& args)
+class Scanner
 {
-    int code = EXIT_FAILURE;
-
-    if (args.size() == 1)
+public:
+    strview takeToken()
     {
-        auto&& a = args[0];
-        from_chars(a.begin(), a.end(), code);
+        size_t i = m_offset;
+        for (; i < m_text.size(); ++i)
+        {
+            if (istoken(m_text[i]))
+                break;
+        }
+
+        size_t j = i;
+        for (; j < m_text.size(); ++j)
+        {
+            if (!istoken(m_text[j]))
+                break;
+        }
+
+        m_offset = j;
+        return m_text.withIndices(i, j);
     }
 
-    exit(code);
-}
-
-bool parseCommand(arglist_t const& args)
-{
-    if (args.empty())
-        return false;
-
-    auto&& command = args[0];
-    if (command == "exit"_sv)
+    strview takeAll()
     {
-        cmdExit(args.subspan(1));
-        return true;
+        auto s = m_text.skipBegin(m_offset);
+        m_offset = s.size();
+        return s;
     }
 
+    Scanner(strparam s)
+        : m_text{s}, m_offset{0}
+    {}
+private:
+    strview m_text;
+    size_t m_offset;
 
-    return false;
-}
+    static bool istoken(char c)
+    {
+        return
+            (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
+            (c >= '0' && c <= '9') || (c >= 33 && c <= 47) ||
+            (c >= 58 && c <= 64) || (c >= 91 && c <= 96) ||
+            (c >= 123 && c <= 126);
+    }
+};
 
 void parseUserInput(strparam s)
 {
-    auto args = trimsplit::containerWS(s);
+    Scanner scanner{s};
 
-    if (!parseCommand(args))
+
+    auto command = scanner.takeToken();
+
+    if (command == "exit"_sv)
     {
-        printf("%.*s: command not found\n", s.size(), s.data());
+        auto arg = scanner.takeAll().trim();
+        int code = 0;
+
+        if (from_chars(arg.begin(), arg.end(), code).ec != errc{})
+            cout << "warning: invalid exit code '" << arg << "\'\n";
+        exit(code);
+    }
+    else if (command == "echo"_sv)
+    {
+        auto arg = scanner.takeAll();
+        cout << arg << "\n";
+    }
+    else
+    {
+        cout << command << ": command not found\n";
     }
 }
 
@@ -64,7 +97,7 @@ int main()
             return EXIT_FAILURE;
         }
 
-        auto user_input = strview(buffer.data(), strlen(buffer.data())-1);
+        auto user_input = cstrview(buffer.data());
 
         parseUserInput(user_input);
 
